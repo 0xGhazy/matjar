ae3fc486b15719d3b7307f1dbb546b15eb054d72
0xGhazy <hhamdypro@gmail.com>
2024-08-25 02:16:14 +0300

    Enable Caching on spring boot application

--- implementation/src/backend/user-management/user-management/src/main/java/com/matjar/user/UserManagementServiceApplication.java
+++ implementation/src/backend/user-management/user-management/src/main/java/com/matjar/user/UserManagementServiceApplication.java
@@ -2,8 +2,10 @@ package com.matjar.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class UserManagementServiceApplication {
 
 	public static void main(String[] args) {
